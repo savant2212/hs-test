420b20a9116126b6ad353469a6d9ddba9ec72437
Nikita Nazarenko <savant.d@gmail.com>
2013-06-26 10:40:38 +0400

    cleanup

--- stulchik.hs
+++ stulchik.hs
@@ -5,28 +5,12 @@ import Data.List
 import Data.Maybe
 import Text.Regex.Base
 import Text.Regex.Posix
-
+import Control.Concurrent
 
 main :: IO ()
 main = do
     http <- simpleHTTP (getRequest "http://stulchik.net/main.shtml?ras") >>= getResponseBody
     
-    putStrLn getAnchor (\x -> (x ~== TagOpen "a" []) && ( fromAttrib "href" x =~ "ras.shtml\\?kat")) $ parseTags(http)
-    
+    let categories = getAnchor (\x -> (x ~== TagOpen "a" []) && ( fromAttrib "href" x =~ "ras.shtml\\?kat")) $ parseTags(http)
 	
-getAnchor :: (Tag str -> Bool) -> [Tag str] -> [(str,Maybe str)]
-getAnchor f [] = []
-getAnchor f [t:ts] = case (f t) of
-                           True  -> (fromAttrib "href" t, maybeTagText $ head ts) : (getAnchor f ts)
-                           False ->  getAnchor f ts
 
-
---  filter (\x -> (x ~== TagOpen "a" []) && fromAttrib "href" x `matches` "ras.shtml\\?kat") $ parseTags(http)
-
---    let tags = dropWhile (~== TagOpen "a" []) (parseTags http)
---    done tags where
---        done xs = case xs of
---            [] -> putStrLn $ "\n"
---            _ -> do
---                putStrLn $ show $ head xs
---                done (tail xs)
